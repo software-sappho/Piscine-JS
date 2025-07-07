dae188187675359d37a357e7f9183db4abd48ee6
astas <andriana.stas00@gmail.com>
2025-07-07 12:31:58 +0300

    mutability.js

---
+++ mutability.js
@@ -0,0 +1,13 @@
+const person = {
+  name: 'Rick',
+  age: 77,
+  country: 'US',
+}
+
+const clone1 = { ...person}
+const clone2 = { ...person}
+
+const samePerson = person;
+
+person.age+=1;
+person.country='FR';
