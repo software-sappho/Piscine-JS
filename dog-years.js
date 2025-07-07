c2aeb40c518b7b5163b500aeb29d0be99e082686
astas <andriana.stas00@gmail.com>
2025-07-07 14:35:37 +0300

    dog-ears.js: just have mercury working

--- dog-years.js
+++ dog-years.js
@@ -3,6 +3,12 @@ function dogYears(planet, age){
         const dogsYearsOnEarth = age / 31557600;
         const dogYearsEarth = dogsYearsOnEarth * 7;
         return dogYearsEarth;
+    }else if (planet === "mercury"){
+        const earthYears = age / 31557600;
+        const mercuryYears = earthYears / 0.2408467;
+        const mercuryDogYears = mercuryYears * 7;
+        return mercuryDogYears;
+
     }
     // You can add other planets later
 }
