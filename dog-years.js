ef22e6b3b3e73e4e33c503e5e182c5661120c335
astas <andriana.stas00@gmail.com>
2025-07-07 13:52:06 +0300

    dog-ears.js: just have earth working

--- dog-years.js
+++ dog-years.js
@@ -1,3 +1,10 @@
-function dogYears(planet,age){
-    
+function dogYears(planet, age){
+    if (planet === "earth") {
+        const dogsYearsOnEarth = age / 31557600;
+        const dogYearsEarth = dogsYearsOnEarth * 7;
+        return dogYearsEarth;
+    }
+    // You can add other planets later
 }
+
+console.log(dogYears("earth", 31557600));  // should print 7
