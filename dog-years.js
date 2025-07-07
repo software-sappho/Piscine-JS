6b9a676d5339ffc97978de3243ae3299a7aab445
astas <andriana.stas00@gmail.com>
2025-07-07 22:46:38 +0300

    dog-ears.js: chatgpt gave me a better solution

--- dog-years.js
+++ dog-years.js
@@ -1,44 +1,17 @@
-function dogYears(planet, age){
-    if (planet === "earth") {
-        const dogsYearsOnEarth = age / 31557600;
-        const dogYearsEarth = dogsYearsOnEarth * 7;
-        return dogYearsEarth;
-    }else if (planet === "mercury"){
-        const earthYears = age / 31557600;
-        const mercuryYears = earthYears / 0.2408467;
-        const mercuryDogYears = mercuryYears * 7;
-        return mercuryDogYears;
-    }else if (planet === "venus"){
-        const earthYears = age / 31557600;
-        const mercuryYears = earthYears / 0.61519726;
-        const mercuryDogYears = mercuryYears * 7;
-        return mercuryDogYears;
-    }else if (planet === "mars"){
-        const earthYears = age / 31557600;
-        const mercuryYears = earthYears / 1.8808158;
-        const mercuryDogYears = mercuryYears * 7;
-        return mercuryDogYears;
-    }else if (planet === "jupiter"){
-        const earthYears = age / 31557600;
-        const mercuryYears = earthYears / 11.862615;
-        const mercuryDogYears = mercuryYears * 7;
-        return mercuryDogYears;
-    }else if (planet === "saturn"){
-        const earthYears = age / 31557600;
-        const mercuryYears = earthYears / 29.447498;
-        const mercuryDogYears = mercuryYears * 7;
-        return mercuryDogYears;
-    }else if (planet === "uranus"){
-        const earthYears = age / 31557600;
-        const mercuryYears = earthYears / 84.016846;
-        const mercuryDogYears = mercuryYears * 7;
-        return mercuryDogYears;
-    }else if (planet === "neptune"){
-        const earthYears = age / 31557600;
-        const mercuryYears = earthYears / 164.79132;
-        const mercuryDogYears = mercuryYears * 7;
-        return mercuryDogYears;
-    }else{
-        return "unsupported planet"
-    }
+const orbitalPeriods = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+function dogYears(planet, age) {
+  const earthYears = age / 31557600;
+  const planetYears = earthYears / orbitalPeriods[planet];
+  const dogYears = planetYears * 7;
+  return Number(dogYears.toFixed(2));
 }
