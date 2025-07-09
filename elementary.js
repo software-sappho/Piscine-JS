9044c70fd3b63d7ec6b2f0276d40a600e4c46623
astas <andriana.stas00@gmail.com>
2025-07-09 13:25:59 +0300

    elementary.js: abs to account for negative numbers?

--- elementary.js
+++ elementary.js
@@ -1,25 +1,35 @@
 function multiply(a,b){
+    let a2=abs(a)
+    let b2=abs(b)
     let result = 0;
-    for (let i = 0; i < b; i++) {
-        result += a;
+    for (let i = 0; i < b2; i++) {
+        result += a2;
     }
     return result
 }
 
 function divide(a,b){
+    let a2=abs(a)
+    let b2=abs(b)
     let result =0;
-    let total = a;
+    let total = a2;
 
-    while (total >= b){
-        total -= b;
+    while (total >= b2){
+        total -= b2;
         result++
     }
     return result
 }
 
 function modulo(a,b){
-    while (a >= b) {
-        a -= b;
+    let a2=abs(a)
+    let b2=abs(b)
+    while (a2 >= b2) {
+        a2 -= b2;
     }
-    return a;
+    return a2;
+}
+
+function abs(number){
+    return number < 0 ? (number*-1) : number;
 }
