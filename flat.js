d5a1b103b62e581f8042024c2e017449fa4c63d4
astas <andriana.stas00@gmail.com>
2025-07-10 18:46:32 +0300

    flat.js

--- flat.js
+++ flat.js
@@ -1,9 +1,9 @@
-function flat(array) {
+function flat(array, depth = 1) {
   let result = [];
 
   for (let item of array) {
-    if (Array.isArray(item)) {
-      result = result.concat(item); // only flatten one level
+    if (Array.isArray(item) && depth > 0) {
+      result = result.concat(flat(item, depth - 1));
     } else {
       result.push(item);
     }
