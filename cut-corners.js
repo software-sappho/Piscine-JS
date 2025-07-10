7d64a31a7d45f610c4574dc14bb26470e10e5d3f
astas <andriana.stas00@gmail.com>
2025-07-10 15:33:32 +0300

    cut-corners.js: yk the drill

--- cut-corners.js
+++ cut-corners.js
@@ -61,7 +61,6 @@
 //I have a piscine to finish okay?
 
 function floor(x) {
-  // floor is the greatest integer less than or equal to x
   if (x >= 0) {
     let i = 0;
     while (i + 1 <= x) {
@@ -78,7 +77,6 @@ function floor(x) {
 }
 
 function ceil(x) {
-  // ceil is the smallest integer greater than or equal to x
   if (x <= 0) {
     let i = 0;
     while (i - 1 >= x) {
@@ -95,7 +93,7 @@ function ceil(x) {
 }
 
 function trunc(x) {
-  // trunc is integer part towards zero
+  // trunc toward zero
   if (x >= 0) {
     let i = 0;
     while (i + 1 <= x) {
@@ -112,16 +110,19 @@ function trunc(x) {
 }
 
 function round(x) {
-  // round to nearest integer, half away from zero (like Math.round)
-  // We'll compare distance to floor and ceil to find which is closer
   let f = floor(x);
   let c = ceil(x);
-  if (x - f < c - x) {
+  let diffFloor = x - f;
+  let diffCeil = c - x;
+
+  const epsilon = 1e-10; // floating precision
+
+  if (diffFloor < 0.5 - epsilon) {
     return f;
-  } else if (x - f > c - x) {
-    return c;
-  } else {
-    // halfway, round away from zero
-    return x >= 0 ? c : f;
   }
+  if (diffFloor > 0.5 + epsilon) {
+    return c;
+  }
+  // diffFloor is approximately 0.5, round away from zero
+  return x >= 0 ? c : f;
 }
