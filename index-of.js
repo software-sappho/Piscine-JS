658357184f02d9ff24824aac773e25304ddb04f7
astas <andriana.stas00@gmail.com>
2025-07-10 13:34:59 +0300

    index-of.js: let's see??

--- index-of.js
+++ index-of.js
@@ -9,9 +9,20 @@ function indexOf(array, value, index){
 }
 
 function lastIndexOf(array, value, index){
-    
+    let i = index || array.length -1
+    for (i; i>=0; i--){
+        if (array[i]===value){
+            return i
+        }
+    }
+    return -1
 }
 
 function includes(array, value){
-    
+    for(let i = 0 ; i<array.length ; i++){
+        if (array[i] === value){
+            return true
+        }
+    }
+    return false
 }
