ccd33cfa029608795848f8ca9cd2fe0cae412d14
astas <andriana.stas00@gmail.com>
2025-07-10 15:11:49 +0300

    cut-corners.js

---
+++ cut-corners.js
@@ -0,0 +1,60 @@
+
+function trunc(num){
+
+    if (num>0){
+    let result = 0
+        while (result+1<=num){
+            result = result+1
+        }
+        return result
+    }
+
+    if (num<0){
+    let result = 0
+    while (result-1>=num){
+        result = result -1
+    }
+    return result
+
+    }
+
+    return 0
+}
+
+
+function round(num){
+    let int= trunc(num)
+    let decimal= num - int
+
+    if (int>=0){
+        if (decimal >= 0.5){
+            return int+1
+        }else{
+            return int
+        }
+    }else{
+        if (decimal <= -0.5) {
+            return int - 1
+        } else {
+            return int
+        }
+    }
+}
+
+function ceil(num){
+    let int=trunc(num)
+    if (num > int) {
+        return int + 1
+    } else {
+        return int
+    }
+}
+
+function floor(num){
+    let int=trunc(num)
+    if (num < int) {
+        return int - 1
+    } else {
+        return int
+    }
+}
