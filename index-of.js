2d9ed5ba265a8cc5a5f87b705c42b065c7d94b05
astas <andriana.stas00@gmail.com>
2025-07-10 13:27:39 +0300

    index-of.js: I am kinda proud actually I actually understand what I am doing

---
+++ index-of.js
@@ -0,0 +1,17 @@
+function indexOf(array, value, index){
+    let i = index || 0
+    for (i; i<array.length; i++) {
+        if (array[i] === value){
+            return i
+        }
+    }
+    return -1
+}
+
+function lastIndexOf(array, value, index){
+    
+}
+
+function includes(array, value){
+    
+}
