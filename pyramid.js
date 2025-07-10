f707fe15bdb72e3c2cdf470a2eca48546b36e31a
astas <andriana.stas00@gmail.com>
2025-07-10 18:36:23 +0300

    pyramid.js:

--- pyramid.js
+++ pyramid.js
@@ -1,10 +1,16 @@
 function pyramid(char, depth) {
+  const maxWidth = (2 * depth - 1) * char.length;
+
   function build(row) {
+    const stars = char.repeat(2 * row - 1);
+    const spaces = ' '.repeat((maxWidth - stars.length) / 2);
+
     if (row === 1) {
-      return ' '.repeat(depth - 1) + char;
+      return spaces + stars;
     } else {
-      return build(row - 1) + '\n' + ' '.repeat(depth - row) + char.repeat(2 * row - 1);
+      return build(row - 1) + '\n' + spaces + stars;
     }
   }
+
   return build(depth);
 }
