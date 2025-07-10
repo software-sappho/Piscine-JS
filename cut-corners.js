8b0b77c3d5789b04e1bfd0b749d8818c99aaf0e1
astas <andriana.stas00@gmail.com>
2025-07-10 15:37:46 +0300

    cut-corners.js: yk the drill2

--- cut-corners.js
+++ cut-corners.js
@@ -60,69 +60,107 @@
 
 //I have a piscine to finish okay?
 
-function floor(x) {
-  if (x >= 0) {
-    let i = 0;
-    while (i + 1 <= x) {
-      i++;
-    }
-    return i;
-  } else {
-    let i = 0;
-    while (i - 1 >= x) {
-      i--;
-    }
-    return i;
-  }
-}
+/*
+Create some functions which behave like JavaScript's Math rounding functions:
 
-function ceil(x) {
-  if (x <= 0) {
-    let i = 0;
-    while (i - 1 >= x) {
-      i--;
+round: which behaves similar to Math.round().
+ceil: which behaves similar to Math.ceil().
+floor: which behaves similar to Math.floor().
+trunc: which behaves similar to Math.trunc().
+Some restrictions apply:
+
+You may not use strings conversion to do it
+No bitwise operator
+No % operator
+*/
+function findNearest(x) {
+    let num = 0;
+    if (x < 1 && x >= 0) return 0
+    if (x < 0 && x >= -1) return 0
+    for (let i = x; i > 1; i = i / 2) {
+        num += 1;
     }
-    return i;
-  } else {
-    let i = 0;
-    while (i + 1 <= x) {
-      i++;
-    }
-    return i;
-  }
+    return num - 1
 }
 
 function trunc(x) {
-  // trunc toward zero
-  if (x >= 0) {
-    let i = 0;
-    while (i + 1 <= x) {
-      i++;
+    let i = 2 ** findNearest(x);
+    if (x >= 0) {
+        for (; i < x;) {
+            i++
+        }
+        if (i === x) return i
+        return i - 1
     }
-    return i;
-  } else {
-    let i = 0;
-    while (i - 1 >= x) {
-      i--;
+    if (x < 0) {
+        i = 0
+        for (; i > x;) {
+            i--
+        }
+        if (x > -1) return -0
+        if (i === x) return i
+        return i + 1
     }
-    return i;
-  }
+}
+
+function floor(x) {
+    let i = 0;
+    if (x >= 0) {
+        for (; i < x;) {
+            i++
+        }
+        if (i === x) return i
+        return i - 1
+    }
+    if (x < 0) {
+        i = 0
+        for (; i > x;) {
+            i--
+        }
+        if (x === -0) return -0
+        if (i === x) return i
+        return i
+    }
+}
+
+function ceil(x) {
+    let i = 0;
+    if (x >= 0) {
+        for (; i < x;) {
+            i++
+        }
+        if (i === x) return i--
+        return i
+    }
+    if (x < 0) {
+        i = 0
+        for (; i > x;) {
+            i--
+        }
+        if (i === x) return i
+        if (i + 1 === 0) return -0
+        return i + 1
+    }
 }
 
 function round(x) {
-  let f = floor(x);
-  let c = ceil(x);
-  let diffFloor = x - f;
-  let diffCeil = c - x;
-
-  const epsilon = 1e-10; // floating precision
-
-  if (diffFloor < 0.5 - epsilon) {
-    return f;
-  }
-  if (diffFloor > 0.5 + epsilon) {
-    return c;
-  }
-  // diffFloor is approximately 0.5, round away from zero
-  return x >= 0 ? c : f;
+    let i = 0;
+    if (x >= 0) {
+        for (; i < x;) {
+            i++
+        }
+        if (i-x > 0.5) return i - 1
+        if (i-x <= 0.5) return i
+    }
+    if (x < 0) {
+        i = 0
+        for (; i > x;) {
+            i--
+        }
+        if (i-x > -0.5) return i
+        if (i-x <= 0.5) {
+            if (i + 1 === 0) return -0
+            return i + 1
+        }
+    }
 }
