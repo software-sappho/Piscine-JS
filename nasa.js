f5d1c747ed66a10535dffc8d1edd384ca7786ba2
astas <andriana.stas00@gmail.com>
2025-07-10 17:27:39 +0300

    nasa.js

---
+++ nasa.js
@@ -0,0 +1,17 @@
+function nasa(N){
+    let result = ""
+
+    for (let i = 1; i<=N; i++){
+        if (i%3 ===0 && i%5===0){
+            result= result + "NASA" + " "
+        }else if(i%3 ===0){
+            result= result + "NA" + " "           
+        }else if(i%5 ===0){
+            result= result + "SA" + " "           
+        }else {
+            result= result + i + " "           
+        }
+    }
+
+    return result
+}
