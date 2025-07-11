4107956e956124452a786fadc4d01e1b124dfdaf
astas <andriana.stas00@gmail.com>
2025-07-11 15:20:29 +0300

    same-ammount.js: added the g flag

--- same-amount.js
+++ same-amount.js
@@ -1,11 +1,19 @@
 function sameAmount(string, regexp1, regexp2){
 
-    let match1 = string.match(regexp1)
-    let match2 = string.match(regexp2)
+    let updatedregexp1= addGlobalFlag(regexp1)
+    let updatedregexp2= addGlobalFlag(regexp2)
+
+    let match1 = string.match(updatedregexp1)
+    let match2 = string.match(updatedregexp2)
 
     if ((match1 || []).length === (match2 || []).length){
         return true
     }else{
         return false
     }
+}
+
+function addGlobalFlag(regexp) {
+  const flags = regexp.flags.includes('g') ? regexp.flags : regexp.flags + 'g';
+  return new RegExp(regexp.source, flags);
 }
