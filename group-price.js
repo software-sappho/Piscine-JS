639956d51ef039c6d01425ab5118bafcc63dfb22
astas <andriana.stas00@gmail.com>
2025-07-11 18:38:08 +0300

    group-price.js: adding for dollar SYMBOLl

--- group-price.js
+++ group-price.js
@@ -1,11 +1,10 @@
-function groupPrice(string){
-    const pattern = /([$A-Z]{1,3})(\d+)\.(\d{2})/g;
-    let matches = [...string.matchAll(pattern)]
+function groupPrice(string) {
+    const pattern = /[$A-Z]{1,3}(\d+)\.(\d{2})/g;
+    let matches = [...string.matchAll(pattern)];
 
-    if (matches.length === 0){
+    if (matches.length === 0) {
         return [];
     }
 
-return matches.map(match => [match[0], match[1], match[2]]);
-
+    return matches.map(match => [match[0], match[1], match[2]]);
 }
