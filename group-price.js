7cf73ae16838c28f9878620b7eb4137bc3ed333b
astas <andriana.stas00@gmail.com>
2025-07-11 18:35:26 +0300

    group-price.js: adding for dollar SYMBOL as well

--- group-price.js
+++ group-price.js
@@ -1,5 +1,5 @@
 function groupPrice(string){
-    const pattern = /\b[A-Z]{1,3}(\d+)\.(\d{2})\b/g;
+    const pattern = /([$A-Z]{1,3})(\d+)\.(\d{2})/g;
     let matches = [...string.matchAll(pattern)]
 
     if (matches.length === 0){
