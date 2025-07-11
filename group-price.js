60970c16b5557623744262541feb2c13fb59999c
astas <andriana.stas00@gmail.com>
2025-07-11 17:22:46 +0300

    group-price.js

---
+++ group-price.js
@@ -0,0 +1,11 @@
+function groupPrice(string){
+    const pattern = /\b[A-Z]{3}(\d+)\.(\d{2})\b/g;
+    let matches = string.matchAll(pattern)
+
+    if (matches.length === 0){
+        return [];
+    }
+
+return matches.map(match => [match[0], match[1], match[2]]);
+
+}
