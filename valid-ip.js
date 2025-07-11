2123123cf80471b1b7a1be5903be06c9cbe6d41b
astas <andriana.stas00@gmail.com>
2025-07-11 18:42:43 +0300

    valid-ip.js

---
+++ valid-ip.js
@@ -0,0 +1,10 @@
+function findIP(string) {
+    const pattern = /\b((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(?!$)){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d))(?::(6553[0-5]|655[0-2]\d|65[0-4]\d{2}|6[0-4]\d{3}|[1-5]?\d{1,4}))?\b/g;
+    let matches = [...string.matchAll(pattern)];
+
+    if (matches.length === 0) {
+        return [];
+    }
+
+    return matches.map(match => match[0]);
+}
