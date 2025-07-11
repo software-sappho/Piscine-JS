ecb9006f8d00df2499972779f94c5d5bf731749a
astas <andriana.stas00@gmail.com>
2025-07-11 14:18:26 +0300

    letter-space-number.js: added numbers as well in regexp at the end

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,7 +1,7 @@
 function letterSpaceNumber(string){
     let result = []
-    const pattern = /[A-Za-z] [0-9](?![a-zA-Z])/g
-    
+    const pattern = /[A-Za-z] [0-9](?![0-9a-zA-Z])/g
+
     
     result = string.match(pattern)
     
