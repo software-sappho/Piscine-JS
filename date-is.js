2c46016b8787647b36572e2e30e825a4eb3652c8
astas <andriana.stas00@gmail.com>
2025-07-14 12:12:41 +0300

    date-is.js: updating isValid to accept nums as well

--- date-is.js
+++ date-is.js
@@ -1,6 +1,12 @@
-function isValid(date){
-    return date instanceof Date && !isNaN(date);
-}   
+function isValid(date) {
+  if (date instanceof Date) {
+    return !isNaN(date);
+  } else if (typeof date === "number") {
+    return !isNaN(new Date(date));
+  } else {
+    return false;
+  }
+}
 
 function isAfter(date1, date2){
     return date1 > date2;
