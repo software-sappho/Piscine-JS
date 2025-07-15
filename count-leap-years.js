354078aac4d2304da6e728c6d48dc30dcbb288a3
astas <andriana.stas00@gmail.com>
2025-07-15 12:22:44 +0300

    count-leap-years.js

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,9 +1,5 @@
-function countLeapYears(date) {
-  const year = date.getFullYear();
-  
-  // Formula:
-  // Leap years = (div by 4) - (div by 100) + (div by 400)
-  const leapYears = Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400);
+"use strict";
 
-  return leapYears;
+function countLeapYears(date) {
+    return Math.ceil(date.getFullYear() / 4) - Math.floor(date.getFullYear() / 100) + Math.floor(date.getFullYear() / 400) - 1
 }
