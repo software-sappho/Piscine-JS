d510415672a1577e760b34d38c4f515c4a50c589
astas <andriana.stas00@gmail.com>
2025-07-17 00:13:25 +0300

    for-each.js: adjusted to fit undefined

--- for-each.js
+++ for-each.js
@@ -1,9 +1,7 @@
 "use strict";
 
 function forEach(arr, f) {
-    const newArr = []
     for (let i = 0; i < arr.length; i++) {
-        newArr.push(f(arr[i], i, arr))
+        f(arr[i], i, arr);
     }
-    return newArr
 }
