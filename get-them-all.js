721359f9cd83e0ed35b9de13d8752d430b66ffa7
astas <andriana.stas00@gmail.com>
2025-07-17 17:52:07 +0300

    get-them-all.js: fixed var declarations to match html's dynamic rendering

--- get-them-all.js
+++ get-them-all.js
@@ -1,43 +1,31 @@
-const architects = Array.from(document.getElementsByTagName('a'));
-
-const nonArchitects = Array.from(document.getElementsByTagName('span'));
-
-const classical = architects.filter(el => el.classList.contains('classical'));
-
-const allAnchors = Array.from(document.getElementsByTagName('a'));
-
-const active = classical.filter(el => el.classList.contains('active'));
-
-
-
 export function getArchitects() {
-
+    const architects = Array.from(document.getElementsByTagName('a'));
+    const nonArchitects = Array.from(document.getElementsByTagName('span'));
     return [architects, nonArchitects];
-
 }
 
 export function getClassical() {
-
+    const architects = Array.from(document.getElementsByTagName('a'));
+    const classical = architects.filter(el => el.classList.contains('classical'));
     const nonClassical = architects.filter(el => !el.classList.contains('classical'));
-
     return [classical, nonClassical];
 }
 
 export function getActive() {
-
+    const architects = Array.from(document.getElementsByTagName('a'));
+    const classical = architects.filter(el => el.classList.contains('classical'));
+    const active = classical.filter(el => el.classList.contains('active'));
     const nonActive = classical.filter(el => !el.classList.contains('active'));
-
     return [active, nonActive];
 }
 
 export function getBonannoPisano() {
     const bonanno = document.getElementById('BonannoPisano');
 
-    const others = active.filter(el => el.id !== 'BonannoPisano');
+    const architects = Array.from(document.getElementsByTagName('a'));
+    const classical = architects.filter(el => el.classList.contains('classical'));
+    const active = classical.filter(el => el.classList.contains('active'));
 
+    const others = active.filter(el => el.id !== 'BonannoPisano');
     return [bonanno, others];
 }
-
-
-
-//npx serve .
