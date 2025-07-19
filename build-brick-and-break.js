aa1f63393a5b7a60383c6aee3b82b2c57d5ac43b
astas <andriana.stas00@gmail.com>
2025-07-19 20:38:58 +0300

    build-brick-and-break.js

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,6 +1,23 @@
-export function build(bricksNumber){
-let bricks = 1
+export function build(bricksNumber) {
+    let bricks = 1
+    var interval = setInterval(() => {
+        var brick = document.createElement('div')
+        brick.id = `brick-${bricks}`
+        if (bricks % 3 === 2) brick.dataset.foundation = 'true'
+        document.body.appendChild(brick)
+        bricks++
+        if (bricks > bricksNumber) clearInterval(interval)
+    }, 100)
+}
 
+export function repair(...ids) {
+    ids.forEach(id => {
+        let element = document.getElementById(id)
+        if (element.dataset.foundation === 'true') element.dataset.repaired = 'in progress'
+        else element.dataset.repaired = 'true'
+    })
+}
 
-
+export function destroy() {
+    document.body.lastElementChild.remove()
 }
