9f8882fe39c6bcf9122df6fb747f61ef5f06cf48
astas <andriana.stas00@gmail.com>
2025-07-23 18:31:29 +0300

    deep-copy.js: ofc special cases for regexp and functions I'm sorry I thought this was a beginner's psicine

--- deep-copy.js
+++ deep-copy.js
@@ -3,7 +3,15 @@ function deepCopy(input){
     if(input === null || typeof input !== 'object'){
         return input;
     }
-    
+
+    if (input instanceof RegExp) {
+    return new RegExp(input.source, input.flags);
+  }
+
+    if (typeof input === 'function') {
+        return input;  // just return the function itself
+    }
+
     let result;
     if (Array.isArray(input)){
         result = [];
