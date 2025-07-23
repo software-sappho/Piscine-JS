d9328ae31904f6dbb5a0fcc41c838d7329e85489
astas <andriana.stas00@gmail.com>
2025-07-23 17:53:06 +0300

    manipulate-entries.js

---
+++ manipulate-entries.js
@@ -0,0 +1,46 @@
+// helpers
+function reduceEntries(obj, callback, initial) {
+  return Object.entries(obj).reduce((acc, [k, v]) => callback(acc, [k, v]), initial);
+}
+
+function filterEntries(obj, callback) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([k, v]) => callback([k, v]))
+  );
+}
+
+function mapEntries(obj, callback) {
+  return Object.fromEntries(
+    Object.entries(obj).map(([k, v]) => callback([k, v]))
+  );
+}
+
+
+
+// main functions
+function totalCalories(cart) {
+  return reduceEntries(cart, (acc, [item, grams]) => {
+    const nutrition = nutritionDB[item];
+    const calPerGram = nutrition.calories / 100;
+    return acc + calPerGram * grams;
+  }, 0);
+}
+
+function lowCarbs(cart) {
+  return filterEntries(cart, ([item, grams]) => {
+    const nutrition = nutritionDB[item];
+    const totalCarbs = nutrition.carbs * grams / 100;
+    return totalCarbs < 50;
+  });
+}
+
+function cartTotal(cart) {
+  return mapEntries(cart, ([item, grams]) => {
+    const nutrition = nutritionDB[item];
+    const scaled = {};
+    for (const key in nutrition) {
+      scaled[key] = +(nutrition[key] * grams / 100).toFixed(2);
+    }
+    return [item, scaled];
+  });
+}
