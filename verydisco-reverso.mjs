caffa3a2f6480ef147e6218bd3bf4afd3e98580a
astas <andriana.stas00@gmail.com>
2025-07-28 13:00:48 +0300

    verydisco-reverso.js: needed to handle sentences

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -5,14 +5,23 @@ const filename = process.argv[2];
 
 const content = await readFile(filename, 'utf8')
 
+const words = content.split(" ");
+
 let result = "";
 
-    const mid = Math.floor(content.length / 2);
-    const firstHalf = content.slice(0, mid);
-    const secondHalf = content.slice(mid);
+for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const mid = Math.floor(word.length / 2);
+    const firstHalf = word.slice(0, mid);
+    const secondHalf = word.slice(mid);
     
-    result = secondHalf + firstHalf;
-
+    result += secondHalf + firstHalf;
+    
+    // Add space only if it's NOT the last word
+    if (i !== words.length - 1) {
+        result += " ";
+    }
+}
 
 console.log(result)
 
