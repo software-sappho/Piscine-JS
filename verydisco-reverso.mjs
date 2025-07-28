1b004fd396a906380ddc547248c5c45275d12553
astas <andriana.stas00@gmail.com>
2025-07-28 12:58:34 +0300

    verydisco-reverso.js: do not needc a for loop, also added Math.floor

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -7,13 +7,12 @@ const content = await readFile(filename, 'utf8')
 
 let result = "";
 
-for (let i = 0; i < content.length; i++) {
-    const mid = Math.ceil(content.length / 2);
+    const mid = Math.floor(content.length / 2);
     const firstHalf = content.slice(0, mid);
     const secondHalf = content.slice(mid);
     
     result = secondHalf + firstHalf;
-}
+
 
 console.log(result)
 
