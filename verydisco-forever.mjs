2b2ea549c2adfb93d284b7b8ed0279643d6a5b74
astas <andriana.stas00@gmail.com>
2025-07-28 12:33:40 +0300

    verydisco-forever.js

---
+++ verydisco-forever.mjs
@@ -0,0 +1,16 @@
+import { writeFileSync } from 'fs';
+
+const args = process.argv[2];
+const words = args.split(" ");
+
+let result = "";
+
+for (let word of words){
+    const mid= Math.ceil(word.length/2);
+    const firstHalf = word.slice(0, mid);
+    const secondHalf = word.slice(mid);
+
+    result += secondHalf + firstHalf + " ";
+}
+
+writeFileSync('verydisco-forever.txt', result);
